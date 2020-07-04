f130fd064f01c58a744a0debc101deb060a64575
K. Davenport <67714704+ITSundae@users.noreply.github.com>
2020-07-04 02:25:46 -0400

    Skeleton crewing the website.

---
+++ login.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <p>Wait did you actually think there was a login page here? Lol.</p>
+    </body>
+</html>
